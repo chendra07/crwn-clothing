a15b765948b6ed0a6a129909202add1e64105f67
Flestnia <48231923+chendra07@users.noreply.github.com>
2021-05-12 22:07:15 +0700

    First Commit
    create react app and add file for homepage

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Reat from "react";
+import './App.css';
+import HomePage from "./homepage.component";
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
