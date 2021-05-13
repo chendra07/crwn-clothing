049806a229c0ac5a4160ae8300caa4b7b2fcbf06
Flestnia <48231923+chendra07@users.noreply.github.com>
2021-05-14 01:32:23 +0700

    styling homepage
    adding image, directory and menu-item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import Reat from "react";
+import React from "react";
+import {Switch, Route, Link} from "react-router-dom";
 import './App.css';
-import HomePage from "./homepage.component";
+import HomePage from "./Pages/homepage/homepage.component";
+
+const HatsPage = props => (
+  <div>
+    <button onClick={() => props.history.push("./hats/topi") }>Topi</button>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
