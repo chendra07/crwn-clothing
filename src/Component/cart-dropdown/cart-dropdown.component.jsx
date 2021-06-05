c490740adadeb87bd9be3965ee2e55280287dfd7
Flestnia <48231923+chendra07@users.noreply.github.com>
2021-06-06 00:40:19 +0700

    add selectors and fix minor bugs

--- src/Component/cart-dropdown/cart-dropdown.component.jsx
+++ src/Component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import {connect} from "react-redux";
+
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
+import {} from "../../redux/cart/cart.selectors";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropDown = () => (
+const CartDropDown = ({CartItems}) => (
     <div className="cart-dropdown">
-        <div className="cart-items"/>
+        <div className="cart-items">
+            {CartItems.map(cartItem => (
+                <CartItem key={CartItem.id} item={cartItem}/>
+            ))}
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
-//13 redux cart
 
-export default CartDropDown;
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    CartItems: cartItems
+});
+
+export default connect(mapStateToProps)(CartDropDown);
--- src/Component/collection-item/collection-item.component.jsx
+++ src/Component/collection-item/collection-item.component.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 
 //redux
-import {connect} from "react-redux";
-import {addItem} from "../../redux/cart/cart.action";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.action";
 
 //component
 import CustomButton from "../custom-button/custom-button.component";
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({item, addItem}) => {
-    const {name, price, imageUrl} = item;
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
 
     return (
         <div className="collection-item">
-            <div 
+            <div
                 className="image"
                 style={{
                     backgroundImage: `url(${imageUrl})`
-                }}/>
+                }} />
             <div className="collection-footer">
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
@@ -25,7 +25,6 @@ const CollectionItem = ({item, addItem}) => {
             <CustomButton onClick={() => addItem(item)} inverted>
                 Add to cart
             </CustomButton>
-    
         </div>
     )
 
